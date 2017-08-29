5390ee9ea4e0088610b36c16b6ff0c73beb71c0d
UtileHomme <jatinsharma@healthheal.in>
2017-08-29 07:43:50 +0530

    Schedule committed

---
+++ Schedule_according_to_things_to_do.php
@@ -0,0 +1,136 @@
+<?php
+
+1. Sunday:
+	- Laravel Videos / PHP newboston videos
+
+	- Atom Editor
+
+2. Monday
+	- GeeksforGeeks (2 Problems and 2 concepts)
+
+	- Revise one old problem everday
+
+	- In office, before 6 (comment LMS code and understand DB schema)
+
+	- In office, do HTML and CSS MDN 1 link(after 6) - done
+
+	- Laravel Documentation one link - done
+
+	- Revise one document from the Revision notes in atom
+
+	- Youtube web development 1 video - done
+
+	- 1 pocket article
+
+	- 1 javatpoint pocket article creation
+
+	- 1 chrome shortcut - done
+
+	- 1 atom shortcut
+
+ 	- 1 atom manual
+
+	- 1 git manual
+
+	- 1 javatpoint html article
+
+	- 1 laravel collective article
+
+	- 1 mysql tuts org links
+
+	- 1 medium article
+
+	- 1 Got link (done)
+
+	- 1 atom cheats code
+
+	- Quizup, Elevate, Memrise
+
+
+3.  Tuesday
+
+	- jQuery Videos (as many as possible)
+
+	- Revise one old problem everday
+
+	- In office, before 6 (comment LMS code understand DB schema)
+
+	- In office,do HTML and CSS MDN 2 links (after 6)
+
+	- Laravel Documentation one link
+
+  	- Revise one document from the Revision notes in atom
+
+	- Youtube web development 1 video
+
+
+4. Wednesday
+
+	-  GeeksforGeeks (2 Problems and 2 concepts)
+
+	- Revise one old problem everday
+
+	- In office, before 6 (comment LMS code understand DB schema)
+
+	- In office, do HTML and CSS MDN 2 links (after 6)
+
+	- Laravel Documentation one link
+
+	- Revise one document from the Revision notes in atom
+
+	- Youtube web development 1 video
+
+
+
+5. Thursday
+
+	- jQuery Videos (as many as possible)
+
+	- Revise one old problem everday
+
+	- In office, before 6 (comment LMS code understand DB schema)
+
+	- In office,do HTML and CSS MDN 2 links(after 6)
+
+	- Laravel Documentation one link
+
+	- Revise one document from the Revision notes in atom
+
+	- Youtube web development 1 video
+
+6. Friday
+
+	-  GeeksforGeeks (2 Problems and 2 concepts)
+
+	- Revise one old problem everday
+
+	- In office, before 6 (comment LMS code understand DB schema)
+
+	- In office, do HTML and CSS MDN 2 links (after 6)
+
+	- Laravel Documentation one link
+
+	- Revise one document from the Revision notes in atom
+
+	- Youtube web development 1 video
+
+7. Saturday
+
+	- jQuery Videos (as many as possible)
+
+	- Revise one old problem everday
+
+	- In office, before 6 (comment LMS code understand DB schema)
+
+	- In office,do HTML and CSS MDN 2 links (after 6)
+
+	- Laravel Documentation one link
+
+	- Revise one document from the Revision notes in atom
+
+	- Youtube web development 1 video
+
+
+Saturday night - binge watch a show or movie till 12 pm
+
+?>
